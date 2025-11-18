78b7919acc19982024d8e5e45a2a58740f51735c
María Ugarte <m.ugartediez@gmail.com>
2025-11-18 17:39:04 +0100

    Java- ventana modal

---
+++ assets/js/404.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector("#modal");
+const modalTrigger = document.querySelector("#modal-trigger");
+
+modalTrigger.addEventListener("click", function () {
+  //Añade un gestor de evento al click
+  modal.classList.remove("hidden");
+});
+
+modal.addEventListener("click", function (event) {
+  //event.target es el objetivo del evento, en este caso es el elemento html donde hemos hecho el click
+
+  if (event.target.classList.contains("modal-background")) {
+    modal.classList.add("hidden");
+  }
+});
